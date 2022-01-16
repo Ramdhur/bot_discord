74f6f089b8fcd8412141f9e02bdb589d13e0fb69
Ramdhur <97690168+Ramdhur@users.noreply.github.com>
2022-01-16 16:25:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import discord
 import os
+import datetime
+
+"""
+Code couleur :
+    - vert : 0x0BD33B
+    - orange : 0xF67B00
+    - rouge : 0xF40000
+
+        elif message.channel.name == "":
+            
+            if message.content.startswith("!"):
+                await message.delete()
+                embed=discord.Embed(
+                    title="",
+                    description = "",
+                    color=0x000000
+                    )
+            else:
+                await message.delete()
+
+"""
 
 class MyClient(discord.Client):
+    
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
 
+#-------------------------------------------------------#        
+        
     async def on_message(self, message):
-        # we do not want the bot to reply to itself
+        # on ne veut pas que le bot se réponde à lui-même
         if message.author.id == self.user.id:
             return
-
+        
+        #--------------------------------------------#    
         if message.channel.name == "test-commandes":
                 
             if message.content.startswith("!radar"):
@@ -21,7 +46,7 @@ class MyClient(discord.Client):
                     embed=discord.Embed(
                     title="Compta de Test",
                     description = "Radar : " + message.content[7:] + "$",
-                    color=0xF6AF00
+                    color=0xF67B00
                     )
                     await message.channel.send(embed=embed)
 
@@ -46,7 +71,23 @@ class MyClient(discord.Client):
             
             else:
                 await message.delete()
-
-
+                
+        #--------------------------------------------#                  
+        elif message.channel.name == "vente-tournée":
+            
+            if message.content.startswith("!export"):
+                await message.delete()
+                if message.content[8:].isnumeric():
+                    date = datetime.datetime.now()
+                    embed=discord.Embed(
+                        title="Système PBSC - Comptabilité - " + str(date.hour) + "h" + str(date.minute),
+                        description = "Export enregister par " + message.author.name + " pour une quantité de : " + message.content[8:] + " !",
+                        color=0x0BD33B
+                        )
+                    await message.channel.send(embed=embed)
+            else:
+                await message.delete()
+            
+        
 client = MyClient()
 client.run(os.environ.get('TOKEN'))
