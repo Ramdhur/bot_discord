3e6ccd206e76754b7022abfb44213d5fb138bae2
Ramdhur <97690168+Ramdhur@users.noreply.github.com>
2022-01-17 16:13:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 import datetime
-import psycopg2
+#import psycopg2
 
 """
 Code couleur :
@@ -69,10 +69,9 @@ class MyClient(discord.Client):
                         color=discord.Colour.orange()
                         )
                     await message.channel.send(embed=embed)
-                    
+            """      
             elif message.content.startswith('!ajouterProduit'):
-                await message.delete()
-                """
+                await message.delete()         
                 HOST = "ec2-34-255-225-151.eu-west-1.compute.amazonaws.com"
                 USER = "bwleptnszvvrct"
                 PASSWORD = "68364d9f99e4bdad10d5c383526d1d271978ba11adbd2b783aaa1faa200221da"
@@ -89,9 +88,9 @@ class MyClient(discord.Client):
                     description = "OK",
                     color=0x000000
                 )
-                """
+                
                 await message.channel.send(embed=embed)    
-                    
+            """
             else:
                 await message.delete()
                 
