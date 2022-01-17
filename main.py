558a7fb02dcdff510e4f065d0207ac37ee3c0fbd
Ramdhur <97690168+Ramdhur@users.noreply.github.com>
2022-01-17 16:09:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import datetime
+import psycopg2
 
 """
 Code couleur :
@@ -68,7 +69,28 @@ class MyClient(discord.Client):
                         color=discord.Colour.orange()
                         )
                     await message.channel.send(embed=embed)
-            
+                    
+            elif message.content.startswith('!ajouterProduit'):
+                await message.delete()
+                
+                HOST = "ec2-34-255-225-151.eu-west-1.compute.amazonaws.com"
+                USER = "bwleptnszvvrct"
+                PASSWORD = "68364d9f99e4bdad10d5c383526d1d271978ba11adbd2b783aaa1faa200221da"
+                DATABASE = "dc4gsphlfb9d1n"
+
+                conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
+                cur = conn.cursor()
+                cur.execute("INSERT INTO compta.produit VALUES ('Truffe',0.1,4,800,800);")
+                conn.commit()
+                cur.close()
+                conn.close()
+                embed=discord.Embed(
+                    title="Test de modif en base",
+                    description = "OK",
+                    color=0x000000
+                )
+                await message.channel.send(embed=embed)    
+                    
             else:
                 await message.delete()
                 
